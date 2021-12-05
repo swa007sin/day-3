372ed7b332054d9abc6e1aacfac5ec1cbe4a9167
Swaraj Singh Chouhan <swarajsingh888.ss@gmail.com>
2021-12-05 14:38:06 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,60 @@
+
+    {
+    "basics"; {
+      "name"; "Swaraj Singh Chouhan",
+      "email"; "swarajsingh888.ss@gmail.com",
+      "phone"; "8778255359",
+      "degree"; "Bsc_Nautical SCience",
+      "location"; {
+        "address"; "gmd 1/b",
+        "postalCode"; "829107",
+        "city"; "Bokaro",
+        "countryCode"; "India",
+        "region"; "Jharkhand"
+      }
+      "profiles"; [
+        {
+          "network": "LinkedIn",
+          "username": "swa007sin"
+        }
+      ]
+    }
+    "work"; [
+      {
+        "company": "BSM",
+        "position": "Deck Cadet",
+        "startDate": "26-01-20221",
+        "endDate": "16-08-2021",
+      }
+    ],
+    "education"; [
+      {
+        "institution": "Indian Maritime University",
+        "area": "Chennai",
+        "studyType": "Full time",
+        "percentage": "83.28",
+      }
+    ],
+    "skills"; [
+      {
+        "name": "Programming language",
+        "level": "Beginner",
+        "keywords": [
+          "Python, Javascript"
+        ]
+      }
+    ],
+    "languages"; [
+      {
+        "language": "Hindi, English",
+      }
+    ],
+    "interests"; [
+      {
+        "name": "Sports",
+        "keywords": [
+          "Cricket"
+        ]
+      }
+    ]
+  }
